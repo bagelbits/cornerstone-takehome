14160b9823a05545a8eb300b08d0bf4639801462
Christopher Ward <cdurianward@gmail.com>
2024-12-01 02:23:29 -0800

    fix(appbar): Pick a slightly nicer color

--- src/components/General/CornerstoneHeader/CornerstoneHeader.tsx
+++ src/components/General/CornerstoneHeader/CornerstoneHeader.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const PageComponent = () => {
   return (
     <Grid item md={12}>
-      <AppBar position="fixed" color="info">
+      <AppBar position="fixed" color="transparent">
         <Box display="flex" sx={{ height: "100%", p: 1 }}>
           <Image src="/logo.png" alt="Logo" width={40} height={40} />
         </Box>
